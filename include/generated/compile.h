fcfd337384e6bae5cc2e2aa207bd879cf45f7051
mzh <1113584652@qq.com>
2026-01-12 10:25:31 +0800

    Remove kernel build artifacts

---
+++ include/generated/compile.h
@@ -0,0 +1,4 @@
+#define UTS_MACHINE		"aarch64"
+#define LINUX_COMPILE_BY	"mzh"
+#define LINUX_COMPILE_HOST	"LAPTOP-LGCRKVO2"
+#define LINUX_COMPILER		"aarch64-linux-gnu-gcc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0, GNU ld (GNU Binutils for Ubuntu) 2.38"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "6.12.0-g6d37f0d15c31-dirty"
